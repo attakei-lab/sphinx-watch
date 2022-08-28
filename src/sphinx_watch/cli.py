534f276307370e96d817c311d044dc21bb4296d6
Kazuya Takei <myself@attakei.net>
2022-08-28 19:29:56 +0900

    feat: Define argument of CLI minimally

--- src/sphinx_watch/cli.py
+++ src/sphinx_watch/cli.py
@@ -1,4 +1,6 @@
 """CLI entrypoint of sphinx-watch."""
+from pathlib import Path
+
 import click
 import sphinx
 
@@ -6,6 +8,20 @@ from . import __version__
 
 
 @click.command()
-def main():
+@click.argument(
+    "source_dir",
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, path_type=Path, resolve_path=True
+    ),
+)
+@click.argument(
+    "build_dir",
+    type=click.Path(
+        exists=True, file_okay=False, dir_okay=True, path_type=Path, resolve_path=True
+    ),
+)
+@click.argument("builder", type=click.STRING)
+def main(source_dir: Path, build_dir: Path, builder: str):
     click.echo(f"sphinx-watch version is {__version__}")
     click.echo(f"Sphinx version is {sphinx.__display_version__}")
+    click.echo(f"Env is:\n\t{source_dir=}\n\t{build_dir=}\n\t{builder=}\n")
